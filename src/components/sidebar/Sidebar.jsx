cde0bec001107d6d3ad496146b68b2b1346b4d98
h4n5o <is1384@googlemail.com>
2024-09-10 12:53:56 +0200

    Sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,57 @@
 import "./sidebar.css"
+import { RssFeed, Chat, Videocam, Groups, Bookmarks, QuestionMark, Work, CalendarToday, School} from "@mui/icons-material"
 
 export default function Sidebar() {
   return (
     <div className="sidebar">
-        Sidebar
+        <div className="sidebarWrapper">
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <RssFeed className="sidebarIcon"/>
+              <span className="sidebarListItemText">Feed</span>
+            </li>
+            <li className="sidebarListItem">
+              <Chat className="sidebarIcon"/>
+              <span className="sidebarListItemText">Chat</span>
+            </li>
+            <li className="sidebarListItem">
+              <Videocam className="sidebarIcon"/>
+              <span className="sidebarListItemText">Videos</span>
+            </li>
+            <li className="sidebarListItem">
+              <Groups className="sidebarIcon"/>
+              <span className="sidebarListItemText">Groups</span>
+            </li>
+            <li className="sidebarListItem">
+              <Bookmarks className="sidebarIcon"/>
+              <span className="sidebarListItemText">Bookmarks</span>
+            </li>
+            <li className="sidebarListItem">
+              <QuestionMark className="sidebarIcon"/>
+              <span className="sidebarListItemText">Questions</span>
+            </li>
+            <li className="sidebarListItem">
+              <Work className="sidebarIcon"/>
+              <span className="sidebarListItemText">Jobs</span>
+            </li>
+            <li className="sidebarListItem">
+              <CalendarToday className="sidebarIcon"/>
+              <span className="sidebarListItemText">Events</span>
+            </li>
+            <li className="sidebarListItem">
+              <School className="sidebarIcon"/>
+              <span className="sidebarListItemText">Courses</span>
+            </li>
+          </ul>
+          <button className="sidebarButton">Show More</button>
+          <hr className="sidebarHr"/>
+          <ul className="sidebarFriendList">
+            <li className="sidebarFriend">
+              <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt=""/>
+              <span className="sidebarFriendName">Jane Doe</span>
+            </li>
+          </ul>
+        </div>
     </div>
   )
 }
